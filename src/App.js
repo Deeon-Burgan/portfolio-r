b10e29d69bad60f8a72e108a3b8148f4a4365ab0
deeonb <deeonburgan@gmail.com>
2021-08-17 22:33:08 -0700

    styling fix in main component, added game pitch component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeaturedGames from './Components/FeaturedGames/FeaturedGames';
 import News from './Components/News/News';
 import ImageBreak from './Components/ImageBreak/ImageBreak';
 import Gallery from './Components/Gallery/Gallery';
+import GamePitch from './Components/GamePitch/GamePitch';
 
 function App() {
   return (
@@ -20,6 +21,11 @@ function App() {
       <ImageBreak image='https://static.wixstatic.com/media/bc9d97_b82bad3f1c38450785da09f6c8862096~mv2.jpg/v1/fill/w_980,h_189,al_c,q_90/archangel_hellfire.webp'/>
       <LineBreak line='Latest Games'/>
       <Gallery />
+      <GamePitch 
+      overlayImage='https://static.wixstatic.com/media/bc9d97_fd690a5bfe5d4ad4a6ea2e85f0f2c02a~mv2.png/v1/crop/x_24,y_110,w_2284,h_1679/fill/w_612,h_440,al_c,q_95/CMAMP_F03_PNG-24_edited.webp'
+      image='https://static.wixstatic.com/media/474ccd_3157b0bb2754425da6f6235aa811e455~mv2.png/v1/fill/w_1903,h_437,al_c,q_95/474ccd_3157b0bb2754425da6f6235aa811e455~mv2.webp' 
+      tagline='Build, Gather, And Fight' 
+      description='Strategic construction, careful resource management, and rapid recruitment are the keys to surviving unrelenting swarms of Nightmares, with robust defences to build, battleworn heroes to enlist, and the tug of war between light and dark to master'/>
     </div>
   );
 }
---
+++ src/Components/GamePitch/GamePitch.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './GamePitch.scss'
+
+export default function GamePitch(props) {
+    return (
+        <div className='gamePitch'>
+            <img src={props.image} id='background' />
+            <div id='gamePitchContent'>
+            <div id='pitchImage'>
+                <img src={'https://static.wixstatic.com/media/bc9d97_fd690a5bfe5d4ad4a6ea2e85f0f2c02a~mv2.png/v1/crop/x_24,y_110,w_2284,h_1679/fill/w_612,h_440,al_c,q_95/CMAMP_F03_PNG-24_edited.webp'} id='overlayImage' />
+            </div>
+            <div id='pitchDescription'>
+                <h3>{props.tagline}</h3>
+                <p>{props.description}</p>
+                <div>Watch Trailer</div>
+            </div>
+            </div>
+        </div>
+    )
+}
